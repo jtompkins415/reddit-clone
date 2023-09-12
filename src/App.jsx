ab8830c1fc3613481dc80f3dbd8cbceae0eb3222
jtompkins415 <john.tompkins415@gmail.com>
2023-09-12 10:02:43 -0700

    Render HeaderNav

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
+import {useState} from 'react';
+import HeaderNav from './components/HeaderNav/HeaderNav'
 import './App.css'
 
 function App() {
   
+  const [currUser, setCurrUser] = useState('');
+
 
   return (
     <>
+    <header>
+      <HeaderNav currUser={currUser} />
+    </header>
      
     </>
   )
